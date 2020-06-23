3ffa1dd08bd88df08d56caf5af5010e5b85b9c35
Raghav <raghav.vuggumudi@gmail.com>
2020-06-22 19:33:29 -0500

    DesignV1

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    private Vector3 move;
+    public GameObject playerLocal;
+    private Player playerScript;
+    private float cameraX;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraX = playerLocal.transform.position.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(cameraX, playerLocal.transform.position.y + 35, playerLocal.transform.position.z - 47);
+    }
+}
